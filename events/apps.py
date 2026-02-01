6d3cf3758668711009a16136a1813f9a00f88e0f
anayethasan <mdneloy256@gmail.com>
2026-02-01 20:03:48 +0600

    try to run server

--- events/apps.py
+++ events/apps.py
@@ -1,5 +1,18 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, render_template
+import os
+from flask import Flask
 
 
 class EventsConfig(AppConfig):
     name = 'events'
+
+app = Flask(__name__)
+
+# Your routes here
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
