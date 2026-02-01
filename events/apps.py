6d4d8ac7c31a0d89c5f73ceb8680bba849b98301
anayethasan <mdneloy256@gmail.com>
2026-02-01 21:28:36 +0600

    i did wrong code port error in deploy now try again

--- events/apps.py
+++ events/apps.py
@@ -1,18 +1,4 @@
-from django.apps import AppConfig, render_template
-import os
-from flask import Flask
-
+from django.apps import AppConfig
 
 class EventsConfig(AppConfig):
     name = 'events'
-
-app = Flask(__name__)
-
-# Your routes here
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
